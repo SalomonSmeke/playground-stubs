e805350a10526005d8fcd5659164bdb40aa67c3d
SalomonSmeke <ssmeke@luc.edu>
2016-06-05 18:54:14 -0500

    Simplified

--- assets/scripts/app/main.js
+++ assets/scripts/app/main.js
@@ -1,10 +1,2 @@
 //Author: Salomon Smeke <salomon@ssmeke.io>
 //Filename: main.js
-
-//Grab pertinent libraries.
-requirejs(['jquery','lodash'], function( $ , _ ) {
-
-    //console.log( $ ); <-- Test for jquery: OK
-    //console.log( _ ); <-- Test for lodash: OK
-
-});
